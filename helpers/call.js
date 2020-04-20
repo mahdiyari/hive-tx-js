2169f389646362d95b3278f18abde8660917bf54
MahdiYari <m.yary16@gmail.com>
2020-04-20 18:03:14 +0430

    add 10s default timeout

--- helpers/call.js
+++ helpers/call.js
@@ -3,22 +3,33 @@ const config = require('../config')
 
 /**
  * Make calls to steem node
- * @param method - e.g. condenser_api.get_dynamic_global_properties
- * @param params - an array
+ * @param {string}method - e.g. condenser_api.get_dynamic_global_properties
+ * @param {Array}params - an array
+ * @param {Number}timeout - optional - default 10 seconds
  */
-const call = async (method, params = []) => {
-  const res = await axios.post(
-    config.node,
-    JSON.stringify({
-      jsonrpc: '2.0',
-      method,
-      params,
-      id: 1
+const call = async (method, params = [], timeout = 10) => {
+  let resolved = 0
+  return new Promise((resolve, reject) => {
+    axios.post(
+      config.node,
+      JSON.stringify({
+        jsonrpc: '2.0',
+        method,
+        params,
+        id: 1
+      })
+    ).then(res => {
+      if (res && res.status === 200) {
+        resolved = 1
+        resolve(res.data)
+      }
     })
-  )
-  if (res && res.status === 200) {
-    return res.data
-  }
+    setTimeout(() => {
+      if (!resolved) {
+        reject(new Error('Network timeout.'))
+      }
+    }, timeout * 1000)
+  })
 }
 
 module.exports = call
