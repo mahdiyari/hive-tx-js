18f0c605a51f554371d12bc2bca1d4b7500f5028
Mahdi Yari <m.yary16@gmail.com>
2020-06-21 13:38:42 +0430

    v3.1.7

--- helpers/Signature.d.ts
+++ helpers/Signature.d.ts
@@ -3,14 +3,7 @@ import { PublicKey } from './PublicKey'
 export class Signature {
   data: Buffer
   recovery: number
-  static fromBuffer(buffer: Buffer): Signature
-  static fromString(string: string): Signature
   constructor(data: Buffer, recovery: number)
-  /**
-   * Recover public key from signature by providing original signed message.
-   * @param message 32-byte message that was used to create the signature.
-   */
-  recover(message: Buffer, prefix?: string): PublicKey
   toBuffer(): Buffer
-  toString(): string
+  customToString(): string
 }
