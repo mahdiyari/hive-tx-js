3514a311b8d82271f5efcfedff51b1c491b2741a
Mahdi Yari <m.yary15@gmail.com>
2023-03-26 18:12:32 +0330

    Update createTransaction.js

--- transactions/createTransaction.js
+++ transactions/createTransaction.js
@@ -1,10 +1,9 @@
-const globalProps = require('../helpers/globalProps')
-const config = require('../config')
+import { getGlobalProps } from '../helpers/globalProps.js'
 
 /** Create transaction by operations */
-const createTransaction = async (operations, exp) => {
+export const createTransaction = async (operations, exp) => {
   const expireTime = exp ? 1000 * exp : 1000 * 60
-  const props = await globalProps()
+  const props = await getGlobalProps()
   const refBlockNum = props.head_block_number & 0xffff
   const refBlockPrefix = Buffer.from(props.head_block_id, 'hex').readUInt32LE(4)
   const expiration = new Date(Date.now() + expireTime)
@@ -19,5 +18,3 @@ const createTransaction = async (operations, exp) => {
     ref_block_prefix: refBlockPrefix
   }
 }
-
-module.exports = createTransaction
