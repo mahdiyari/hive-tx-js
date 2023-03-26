afc2e2652e4d2219e00ea0da0435b7a3cbdeabbe
Mahdi Yari <m.yary15@gmail.com>
2023-03-26 18:12:20 +0330

    Update broadcastTransaction.js

--- transactions/broadcastTransaction.js
+++ transactions/broadcastTransaction.js
@@ -1,11 +1,9 @@
-const call = require('../helpers/call')
+import { call } from "../helpers/call.js"
 
 /** Broadcast signed transaction */
-const broadcastTransaction = async signedTransaction => {
+export const broadcastTransaction = async signedTransaction => {
   const result = await call('condenser_api.broadcast_transaction', [
     signedTransaction
   ])
   return result
 }
-
-module.exports = broadcastTransaction
