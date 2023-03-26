fad08c043507036aa0e586e0df89c636983f9437
Mahdi Yari <m.yary15@gmail.com>
2023-03-26 18:12:06 +0330

    Update Signature.d.ts

--- helpers/Signature.d.ts
+++ helpers/Signature.d.ts
@@ -1,10 +1,13 @@
 import { PublicKey } from './PublicKey'
 
+/** ECDSA (secp256k1) signature. */
 export class Signature {
   data: Buffer
   recovery: number
   constructor(data: Buffer, recovery: number)
   toBuffer(): Buffer
+  /** String representation of the Signature */
   customToString(): string
+  /** Create a Signature from string */
   static from(data: string): Signature
 }
