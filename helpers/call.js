7e21f927bc46d8a1e71707db2a0b5195febe84ac
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2024-12-17 16:01:59 +0330

    call.js: timeout and failover and retry

--- helpers/call.js
+++ helpers/call.js
@@ -1,28 +1,57 @@
 import axios from 'axios'
 import { config } from '../config.js'
 
+let nodeIndex = 0
+let numTries = 0
+
 /**
- * Make calls to hive node
+ * Make calls to hive node and retry - Only if provided config.node is an array
  * @param {string}method - e.g. condenser_api.get_dynamic_global_properties
  * @param {[any]|object}params - an array or object
- * @param {number}timeout - optional - default 10 seconds
+ * @param {number}timeout - optional - default 5 seconds
+ * @param {number}retry - optional - default 5 retries before throw
  */
-export const call = async (method, params = [], timeout = 10) => {
-  let resolved = 0
+export const call = async (method, params = [], timeout = config.timeout, retry = config.retry) => {
+  let node = ''
+  if (Array.isArray(config.node) && config.node.length > 0) {
+    node = config.node[nodeIndex]
+  } else {
+    node = config.node
+  }
+  const body = JSON.stringify({
+    jsonrpc: '2.0',
+    method,
+    params,
+    id: 1
+  })
+  try {
+    const res = await callWithTimeout(node, body, timeout)
+    numTries = 0
+    return res
+  } catch (e) {
+    nodeIndex++
+    numTries++
+    if (!Array.isArray(config.node) || numTries > retry) {
+      throw e
+    }
+    if (nodeIndex > config.node.length - 1) {
+      nodeIndex = 0
+    }
+    return call(method, params, timeout, retry)
+  }
+}
+
+const callWithTimeout = (url, body, timeout) => {
   const conf = {}
   if (config.axiosAdapter !== null) {
     conf.adapter = config.axiosAdapter
   }
+  let resolved = 0
   return new Promise((resolve, reject) => {
     axios
       .post(
-        config.node,
-        JSON.stringify({
-          jsonrpc: '2.0',
-          method,
-          params,
-          id: 1
-        }),
+        url,
+        body,
         conf
       )
       .then(res => {
@@ -30,10 +59,12 @@ export const call = async (method, params = [], timeout = 10) => {
           resolved = 1
           resolve(res.data)
         }
+      }).catch(e => {
+        reject(e)
       })
     setTimeout(() => {
       if (!resolved) {
-        reject(new Error('Network timeout.'))
+        reject(new Error(`Network timeout: ${url}: ${body}`))
       }
     }, timeout * 1000)
   })
