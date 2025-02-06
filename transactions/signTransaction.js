1575fd0c0d33709da301a6d58638b69e7ef09af6
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2025-02-06 06:53:00 +0330

    remove buffer

--- transactions/signTransaction.js
+++ transactions/signTransaction.js
@@ -1,9 +1,11 @@
-import ByteBuffer from 'bytebuffer-hex-custom'
+import { ByteBuffer } from '../helpers/ByteBuffer.js'
+// import ByteBuffer2 from 'bytebuffer-hex-custom'
 import { config } from '../config.js'
 import { sha256 } from '../helpers/crypto.js'
 import { Serializer } from '../helpers/serializer.js'
+import { hexToUint8Array, uint8ArrayToHex } from '../helpers/uint8Array.js'
 
-const CHAIN_ID = Buffer.from(config.chain_id, 'hex')
+const CHAIN_ID = hexToUint8Array(config.chain_id)
 
 /**
  * Sign a transaction by keys (supports multi signature)
@@ -11,7 +13,6 @@ const CHAIN_ID = Buffer.from(config.chain_id, 'hex')
  * @param keys - Array of keys<Buffer>
  */
 export const signTransaction = (transaction, keys) => {
-  const CHAIN_ID = Buffer.from(config.chain_id, 'hex')
   const { digest, txId } = transactionDigest(transaction, CHAIN_ID)
   const signedTransaction = { ...transaction }
   if (!signedTransaction.signatures) {
@@ -34,15 +35,24 @@ export const transactionDigest = (transaction, chainId = CHAIN_ID) => {
     ByteBuffer.DEFAULT_CAPACITY,
     ByteBuffer.LITTLE_ENDIAN
   )
+  // const buffer2 = new ByteBuffer2(
+  //   ByteBuffer2.DEFAULT_CAPACITY,
+  //   ByteBuffer2.LITTLE_ENDIAN
+  // )
   const temp = { ...transaction }
+  // const temp2 = { ...transaction }
   try {
     Serializer.Transaction(buffer, temp)
+    // Serializer.Transaction(buffer2, temp2)
   } catch (cause) {
     throw new Error('Unable to serialize transaction: ' + cause)
   }
   buffer.flip()
-  const transactionData = Buffer.from(buffer.toBuffer())
-  const txId = sha256(transactionData).toString('hex').slice(0, 40)
-  const digest = sha256(Buffer.concat([chainId, transactionData]))
+  // console.log(buffer.toBuffer())
+  // console.log(buffer2.toBuffer())
+  const transactionData = new Uint8Array(buffer.toBuffer())
+  // console.log(uint8ArrayToHex(transactionData))
+  const txId = uint8ArrayToHex(sha256(transactionData)).slice(0, 40)
+  const digest = sha256(new Uint8Array([...chainId, ...transactionData]))
   return { digest, txId }
 }
