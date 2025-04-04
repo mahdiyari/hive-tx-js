cc24615f3abe22c8a29c16d9328bc6972d7abdad
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2025-04-04 10:00:29 +0330

    unref the healtcheck interval so the app can exit if there is nothing else to do

--- helpers/call.js
+++ helpers/call.js
@@ -127,7 +127,7 @@ const changeNode = async (newNodeIndex = nodeIndex + 1) => {
 }
 
 // Periodic healthcheck of the current node every 30s
-setInterval(async () => {
+const healthcheck = setInterval(async () => {
   if (!Array.isArray(config.node)) {
     return
   }
@@ -148,3 +148,6 @@ setInterval(async () => {
     changeNode()
   }
 }, config.healthcheckInterval)
+
+// Don't keep the app active just because of this interval
+healthcheck.unref()
