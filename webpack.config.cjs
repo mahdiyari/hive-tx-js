5371c5a632e624e6c491223cbecc80a37530a6b3
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2025-11-28 04:52:34 +0330

    Create webpack.config.cjs

---
+++ webpack.config.cjs
@@ -0,0 +1,22 @@
+const path = require('path')
+const webpack = require('webpack')
+
+// We define two separate configurations to run simultaneously
+module.exports = [
+  {
+    entry: './src/index.ts',
+    output: {
+      filename: 'hive-tx.min.js',
+      path: path.resolve(__dirname, 'dist/browser'),
+      library: 'hiveTx',
+      libraryTarget: 'umd',
+      iife: true
+    },
+    devtool: 'source-map',
+    resolve: { extensions: ['.ts'] },
+    module: {
+      rules: [{ test: /\.ts$/, use: 'ts-loader' }]
+    },
+    mode: 'production'
+  }
+]
