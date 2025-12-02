4b5c7d7de549ad66d1a6f84148106759db4dbe9b
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2025-12-02 04:00:40 +0330

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,3 @@
-import { PrivateKey } from './helpers/PrivateKey'
-import { PublicKey } from './helpers/PublicKey'
-import { Signature } from './helpers/Signature'
-import { call } from './helpers/call'
-import { config } from './config'
-import { Memo } from './helpers/memo'
-import * as utils from './helpers/utils'
-import { Transaction } from './Transaction'
-
 export { Transaction } from './Transaction'
 export { PrivateKey } from './helpers/PrivateKey'
 export { call } from './helpers/call'
@@ -15,14 +6,3 @@ export { PublicKey } from './helpers/PublicKey'
 export { Signature } from './helpers/Signature'
 export { Memo } from './helpers/memo'
 export * as utils from './helpers/utils'
-
-export default {
-  Transaction,
-  PrivateKey,
-  call,
-  config,
-  PublicKey,
-  Signature,
-  Memo,
-  utils
-}
