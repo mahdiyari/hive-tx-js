a11643cf6907466355f0b751fc618b7756f993e7
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2025-12-04 02:29:36 +0330

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 export { Transaction } from './Transaction'
 export { PrivateKey } from './helpers/PrivateKey'
-export { call } from './helpers/call'
+export { callRPC, callREST } from './helpers/call'
 export { config } from './config'
 export { PublicKey } from './helpers/PublicKey'
 export { Signature } from './helpers/Signature'
 export { Memo } from './helpers/memo'
 export * as utils from './helpers/utils'
+
+// Export all types
+export * from './types'
