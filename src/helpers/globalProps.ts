339c0a5a213e1b181e3e8922035ff0f1d119e7c1
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2025-12-04 02:29:32 +0330

    Update globalProps.ts

--- src/helpers/globalProps.ts
+++ src/helpers/globalProps.ts
@@ -1,8 +1,8 @@
-import { call } from './call'
+import { callRPC } from './call'
 
 /** return global properties */
 export const getGlobalProps = async () => {
-  const res = await call('condenser_api.get_dynamic_global_properties', [])
+  const res = await callRPC('condenser_api.get_dynamic_global_properties', [])
   if (!res) {
     throw new Error("Couldn't resolve global properties")
   }
