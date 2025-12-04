5a4378ddea9ad33bc8a94b45bdcf3ae01c5be94c
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2025-12-04 21:57:18 +0330

    Update getTransactionHex.ts

--- tests/getTransactionHex.ts
+++ tests/getTransactionHex.ts
@@ -1,12 +1,12 @@
 import { sha256 } from '@noble/hashes/sha2.js'
-import { call, config, TransactionType } from 'hive-tx'
+import { callRPC, config, TransactionType } from 'hive-tx'
 import { bytesToHex, hexToBytes } from '@noble/hashes/utils.js'
 const CHAIN_ID = hexToBytes(config.chain_id)
 
 export const getTransactionHex = async (transaction: TransactionType) => {
-  const res = await call('condenser_api.get_transaction_hex', [transaction])
-  if (res && res.result) {
-    const transactionData = hexToBytes(res.result).subarray(0, -1)
+  const res = await callRPC('condenser_api.get_transaction_hex', [transaction])
+  if (res) {
+    const transactionData = hexToBytes(res).subarray(0, -1)
     const txId = bytesToHex(sha256(transactionData)).slice(0, 40)
     const digest = sha256(new Uint8Array([...CHAIN_ID, ...transactionData]))
     return { digest, txId }
